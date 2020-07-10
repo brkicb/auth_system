0dfeca026a471e99ba174762405d01005ac33ec8
Bryan Brkic <bryan@MacBook-Pro.local>
2020-07-10 16:09:36 -0400

    Add check authorization in layout

--- src/hocs/Layout.js
+++ src/hocs/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { connect } from 'react-redux';
+import { checkAuthenticated } from '../actions/auth';
 
 const layout = (props) => (
     <div>
+        {props.checkAuthenticated()}
         <Navbar />
         {props.children}
     </div>
 );
 
-export default layout;
+export default connect(null, { checkAuthenticated })(layout);
